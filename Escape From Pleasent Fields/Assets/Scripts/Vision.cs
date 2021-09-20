737bf6ce95d0fcbf62dca5d037d93014ee86f0a3
Mediocrity0 <Zfranklin270@gmail.com>
2021-09-20 19:45:39 +1000

    Ai line of sight without angle - zach
    Added ai script that checks if the player is close enough and if line of sight to the player is obstructed.

---
+++ Escape From Pleasent Fields/Assets/Scripts/Vision.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vision : MonoBehaviour
+{
+    [SerializeField]
+    public LayerMask interactable;
+    [SerializeField]
+    [Tooltip("How far can it see")]
+    public float seeDistance;
+
+    Transform player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(transform.position, transform.forward * 1, Color.magenta);
+
+        RaycastHit lineinfo;
+        Physics.Linecast(transform.position, player.transform.position, out lineinfo, interactable);
+
+        //Calculate distance without y axis. 
+        Vector3 distance = transform.position - new Vector3(player.position.x, transform.position.y, player.position.z);
+        float Distance = distance.magnitude;
+        Debug.Log(Distance);
+        
+        
+
+        //check if the player is within vision range of the nurse.
+        if (Distance < seeDistance && lineinfo.collider.tag == "Player")
+        {
+            Debug.DrawLine(transform.position, player.position, Color.blue);
+        }
+        else
+        {
+            Debug.DrawLine(transform.position, player.position, Color.red);
+        }
+
+
+    }
+}
