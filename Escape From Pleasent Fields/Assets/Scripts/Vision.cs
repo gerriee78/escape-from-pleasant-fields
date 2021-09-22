e0653b09906e952d816a2b57e7b7ca2d75025602
Mediocrity0 <Zfranklin270@gmail.com>
2021-09-22 15:26:22 +1000

    Added visuals to AI vision - zach

--- Escape From Pleasent Fields/Assets/Scripts/Vision.cs
+++ Escape From Pleasent Fields/Assets/Scripts/Vision.cs
@@ -11,21 +11,40 @@ public class Vision : MonoBehaviour
     public float seeDistance;
     public float visionAngle;
 
+    [SerializeField]
+    GameObject visionCone;
+    Material coneMat;
     Transform player;
+    [SerializeField]
+    LayerMask visionblocker;
+    
     
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        Renderer coneRender = visionCone.GetComponent<Renderer>();
+        coneMat = coneRender.material;
+        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Linecast to the player to check if the player is within vision 
         Debug.DrawRay(transform.position, transform.forward * 1, Color.magenta);
-
         RaycastHit lineinfo;
         Physics.Linecast(transform.position, player.transform.position, out lineinfo, interactable);
+
+        //Update vision cone shader to match set angle and distance.
+        coneMat.SetFloat("Angle", Mathf.Lerp(-50, 0, Mathf.InverseLerp(0, 180, visionAngle)));
+        visionCone.transform.localScale = new Vector3(12.5f * seeDistance, 12.5f * seeDistance, visionCone.transform.localScale.z);
+
+       
+        
+
+
 
         //Calculate distance without y axis. 
         Vector3 distance = transform.position - new Vector3(player.position.x, transform.position.y, player.position.z);
@@ -33,12 +52,10 @@ public class Vision : MonoBehaviour
         //Debug.Log(Distance);
         
         //Calculate angle without y axis.
-        float angle = Vector3.Angle(Vector3.forward, -distance);
+        float angle = Vector3.Angle(transform.forward, -distance);
         Debug.Log(angle);
 
-
-
-        //check if the player is within vision range of the nurse.
+        //if the player is seens
         if (Distance < seeDistance && lineinfo.collider.tag == "Player" && angle < visionAngle)
         {
             Debug.DrawLine(transform.position, player.position, Color.blue);
@@ -48,4 +65,7 @@ public class Vision : MonoBehaviour
             Debug.DrawLine(transform.position, player.position, Color.red);
         }
     }
+
+   
+
 }
