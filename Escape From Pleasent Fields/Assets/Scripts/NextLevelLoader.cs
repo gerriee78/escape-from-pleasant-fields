2f724ee2439bbfe3c1875a27d42f9d7698e1285b
Mediocrity0 <Zfranklin270@gmail.com>
2021-10-06 19:45:32 +1100

    added trigger prefab for loading the next level. - zach

---
+++ Escape From Pleasent Fields/Assets/Scripts/NextLevelLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevelLoader : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.sceneCount + 1);
+        }
+
+
+    }
+
+}
