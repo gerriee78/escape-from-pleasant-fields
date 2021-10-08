538e88d4c74e4a3d46acc8417abc03a2f13854a7
DashTheSquid <88768687+DashTheSquid@users.noreply.github.com>
2021-10-08 14:59:54 +1100

    Morgan Timer

---
+++ Escape From Pleasent Fields/Assets/Scripts/TimerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public float timer = 10f;
+    private Text timerSeconds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerSeconds = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        timerSeconds.text = timer.ToString("f0");
+        if (timer <- 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
+
