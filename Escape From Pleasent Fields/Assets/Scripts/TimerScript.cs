14b942e34aab00c383ede2a0a762ab2c1cc13a1d
Mediocrity0 <Zfranklin270@gmail.com>
2021-10-13 06:09:13 +1100

    Ported timer to player prefab - zach

--- Escape From Pleasent Fields/Assets/Scripts/TimerScript.cs
+++ Escape From Pleasent Fields/Assets/Scripts/TimerScript.cs
@@ -8,6 +8,7 @@ public class TimerScript : MonoBehaviour
 {
     public float timer = 10f;
     private Text timerSeconds;
+    private float PreviousNumber; 
 
     // Start is called before the first frame update
     void Start()
@@ -19,11 +20,29 @@ public class TimerScript : MonoBehaviour
     void Update()
     {
         timer -= Time.deltaTime;
-        timerSeconds.text = timer.ToString("f0");
+        //timerSeconds.text = timer.ToString("f0");
         if (timer <- 0)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
+
+        //show number with decimals (this is just to make it seem faster/more urgent)
+        int FrontNum = Mathf.RoundToInt(timer);
+        float Number = Mathf.Round(timer*10) / 10;
+
+        float difference = timer - PreviousNumber;
+
+        if (Number.ToString().Length - FrontNum.ToString().Length == 0)
+        {
+            timerSeconds.text = (FrontNum.ToString() + ".0");
+        }
+        else
+        {
+            timerSeconds.text = Number.ToString();
+        }
+
+
+
     }
 }
 
