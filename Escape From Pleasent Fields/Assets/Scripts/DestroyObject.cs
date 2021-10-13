e4e02b7c326fb67e7f198ca0893261e15e7f8a0b
DashTheSquid <88768687+DashTheSquid@users.noreply.github.com>
2021-10-13 17:24:13 +1100

    Level 3 key and exit

---
+++ Escape From Pleasent Fields/Assets/Scripts/DestroyObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour
+{
+    public GameObject objToDestroy;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+            DestroyObject(objToDestroy);
+
+    }
+}
