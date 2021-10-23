31ba98218f53dcfc273bc2de9c8957ad6d03adae
Mediocrity0 <Zfranklin270@gmail.com>
2021-10-23 16:48:27 +1100

    Final level 1 adjustments - zach

---
+++ Escape From Pleasent Fields/Assets/Scripts/InfoBox.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class InfoBox : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI tmp;
+    [SerializeField]
+    GameObject Textbox;
+    [SerializeField]
+    string message;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Textbox.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Textbox.SetActive(true);
+            tmp.text = message; 
+        }
+       
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Textbox.SetActive(false);
+    }
+}
